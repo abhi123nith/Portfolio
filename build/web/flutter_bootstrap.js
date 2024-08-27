3c4e4e2a244931e3bf4a2ef45308ad7e8dc658fd
abhi123nith <22bcs007@nith.ac.in>
2024-08-27 13:16:16 +0530

    change

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "148868343"
+    serviceWorkerVersion: "822742544"
   }
 });

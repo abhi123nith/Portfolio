65c1ff021e7231992fe5afe5eb7ca584e237e6b5
abhi123nith <22bcs007@nith.ac.in>
2024-08-27 13:37:33 +0530

    commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8c5859269f99e6ed7cb6483800a1a1ee
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "68eeab4e24b7b055b87558ba1398021b",
+"flutter_bootstrap.js": "2b46ad6c3d6cea0ef5a48cd3f61af5ba",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
